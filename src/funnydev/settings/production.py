2057de381f95115b4c8f745999ecee0077c1f4b6
ejarquin <ejarquin@thedataage.com>
2016-11-22 11:51:31 -0600

    logre hacer que se cargaran los staticos en produccion instalando la libreria whitenoise

--- src/funnydev/settings/production.py
+++ src/funnydev/settings/production.py
@@ -29,6 +29,10 @@ TEMPLATES = [
         },
     },
 ]
+
+MIDDLEWARE_CLASSES += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
