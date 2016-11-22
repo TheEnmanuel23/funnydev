75a6d16a8886c59d288038ef510942a1bb241d93
ejarquin <ejarquin@thedataage.com>
2016-11-22 11:53:02 -0600

    puse la configuracion de los staticos en el archivo base.py y los quite de loca y production

--- src/funnydev/settings/production.py
+++ src/funnydev/settings/production.py
@@ -33,13 +33,3 @@ TEMPLATES = [
 MIDDLEWARE_CLASSES += [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
