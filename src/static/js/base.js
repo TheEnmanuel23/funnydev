fd3ad9da86f9563f6b41695aa2e134b959cc9a0a
enmisac <enmisac@gmail.com>
2016-12-01 22:08:10 -0600

    quite los archivos de bootstrap y logre hacer que se mostrara y ocultara el campo de busqueda

---
+++ src/static/js/base.js
@@ -0,0 +1,36 @@
+$(function(){
+	var $body = $('body');
+	var $post = $('.post-list-container .post-container');
+	$post.hover(function(e){
+		$body.css('overflow-y', 'hidden');
+		var $background_trans = $('.post-container .post-zoom .background-trans');
+		$current_post_readmore_container = $(e.currentTarget).find('.container-read-more');
+		$current_post_hover = $(e.currentTarget).find('.post-zoom').find('.background-trans');
+
+		$background_trans.toggleClass('opacity_post');
+		$current_post_readmore_container.toggleClass('opacity_readmore');
+
+		$('.post .date').toggleClass('color_gray_post');
+		$('.post .title').toggleClass('color_gray_post');
+		$('.post .author .createby').toggleClass('color_gray_post');
+		$('.post .read-more i').toggleClass('color_gray_post');
+
+
+		$current_post_hover.removeClass('opacity_post');
+		$(e.currentTarget).find('.post').find('.date').removeClass('color_gray_post');
+		$(e.currentTarget).find('.post').find('.title').removeClass('color_gray_post');
+		$(e.currentTarget).find('.post').find('.author .createby').removeClass('color_gray_post');
+		$(e.currentTarget).find('.post').find('.read-more i').removeClass('color_gray_post');
+	});
+
+	var $find_control_container = $('.find-control-container');
+	var $find_icon_container = $find_control_container.find('.container-icon-search');
+	var $icon_search = $find_icon_container.find('i');;
+
+
+
+	$icon_search.click(function(){
+		var $input = $find_control_container.find('.search');
+		$input.fadeToggle( 'fast');
+	});
+});
